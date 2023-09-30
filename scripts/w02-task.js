d678d3d185a939869f6a3744821bdff63d7bf747
Seckin Akar <seckinakar09@gmail.com>
2023-09-30 14:51:31 +0300

    Corrected

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -4,8 +4,8 @@
 
 /* Step 2 - Variables */
 let fullName = "Seçkin Akar";                           //1
-let currentYear = Date().getFullYear();                 //2
-let profilePicture = "images/photo.jpg";                //3
+let currentYear = new Date().getFullYear();             //2
+let profilePicture = "images/myphoto.jpg";              //3
 /* Step 3 - Element Variables */
 const nameElement = document.getElementById("name");    //1
 const foodElement = document.getElementById("food");    //2
@@ -17,18 +17,16 @@ const imageElement = document.querySelector("img");     //4
 nameElement.innerHTML = `<strong>${fullName}</strong>`; //1 & 2
 yearElement.textContent = currentYear;                  //3
 imageElement.setAttribute("src", profilePicture);
-imageElement.setAttribute(`"alt", "Profile image of ${fullName}"`);
+imageElement.setAttribute("alt", `"Profile image of ${fullName}"`);
 
 
 /* Step 5 - Array */
-let favFoods = ["Yogurt", "Cigkofte", "Doner", "Baklava", "Pineapple", "Mango", "Papaya", "Beans"]  //1
-foodElement.textContent = favFoods;
-//2 ???? I did not understand the instruction.
-//document.querySelector("#food").textContent = favFoods;
-let anotherFavFood = "Pineapple"                            //3
-favFoods.push(anotherFavFood);                              //4
-foodElement.innerHTML += `<br>${favFoods}`;                 //5
-favFoods.shift();                                           //6
-foodElement.innerHTML += `<br>${favFoods}`;                 //7
-favFoods.pop();                                             //8
-foodElement.innerHTML += `<br>${favFoods}`;                 //9
+let favFoods = ["Yogurt", "Cigkofte", "Doner", "Baklava", "Pineapple", "Mango", "Papaya", "Beans"];  //1
+foodElement.textContent = favFoods.join(", ");
+let anotherFavFood = "Pineapple";                       //3
+favFoods.push(anotherFavFood);                          //4
+foodElement.innerHTML += `<br>${favFoods.join(", ")}`;  //5
+favFoods.shift();                                       //6
+foodElement.innerHTML += `<br>${favFoods.join(", ")}`;  //7
+favFoods.pop();                                         //8
+foodElement.innerHTML += `<br>${favFoods.join(", ")}`;  //9
