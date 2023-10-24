d0c89a55a885db891e96b501070f67d45e4e517f
Seckin Akar <seckinakar09@gmail.com>
2023-10-24 06:18:06 +0300

    Fixed the EventListener

--- scripts/w05-task.js
+++ scripts/w05-task.js
@@ -1,68 +1,74 @@
 /* W05: Programming Tasks */
 
 /* Declare and initialize global variables */
-
-const templesElement = document.querySelector("#temples");
+let templesElement = document.querySelector('#temples');
 let templeList = [];
 
 /* async displayTemples Function */
-
 const displayTemples = (temples) => {
-    templeList.forEach(temple => {
-        const articleElement = document.createElement("article");
-        const nameH3Element = document.createElement('h3'); 
-        nameH3Element.textContent = temple.templeName;
-        const imgElement = document.createElement("img");
-        imgElement.src = temple.imageUrl;
-        imgElement.alt = temple.location;
-        articleElement.appendChild(nameH3Element);
-        articleElement.appendChild(imgElement);
-        templesElement.appendChild(articleElement);
-    });
-}
 
-/* async getTemples Function using fetch()*/
-
-const getTemples = async() => {
-    const response = await fetch("https://byui-cse.github.io/cse121b-ww-course/resources/temples.json");
-    if (response.ok)
+    temples.forEach((temple) =>
     {
-        templeList = await response.json();
+        const articleElement = document.createElement('article');
+        //articleElement.style.backgroundColor = "";
+
+        const h3Element = document.createElement('h3')
+        h3Element.textContent += temple.templeName;
+
+        const imgElement = document.createElement ('img');
+        imgElement.setAttribute ('src', `${temple.imageUrl}`);
+        imgElement.setAttribute ('alt', temple.templeName);
+        imgElement.setAttribute ('width', "270em");
+        //imgElement.setAttribute ('height', "400");
+
+        articleElement.appendChild (h3Element);
+        articleElement.appendChild (imgElement);
+        templesElement.appendChild (articleElement);
+    });
+}; 
+/* async getTemples Function using fetch()*/
+const getTemples = async () => {
+    const response = await fetch ('https://byui-cse.github.io/cse121b-ww-course/resources/temples.json');
+
+    if (response. ok)
+    {
+        templeList = await response.json ();
     
-        displayTemples(templeList);
+        displayTemples (templeList);
     }
 }
 
+
 /* reset Function */
-const reset = function() {
-    templesElement.articleElement = ""
+function reset () {
+    templesElement.textContent = '';
 }
+
 /* sortBy Function */
-const sortBy = function (temples) {
-    reset();
-    let filter = document.querySelector("#sortBy").value;
+function sortBy (temples) {
+
+    reset ();
+
+    const filter = document.querySelector ('#sortBy').value;
+
     switch (filter) {
         case "utah":
             displayTemples(temples.filter(temp => temp.location.includes("Utah")));
             break;
         case "notutah":
             displayTemples(temples.filter(temp => !temp.location.includes("Utah")));
+            break;
         case "older":
             displayTemples(temples.filter(temp => new Date(temp.dedicated) < new Date(1950, 0, 1)));
             break;
         case "all":
-            displayTemples(temples)
+            displayTemples(temples);
+            break;
         default:
             console.log("Invalid filter option");
     }
 }
 
-
-
-
 /* Event Listener */
-document.querySelector("#sortBy").addEventListener("change", () => {sortBy(templeList)});
-
-
-getTemples();
-
+document.querySelector("#sortBy").addEventListener("change",() => {sortBy(templeList)});
+getTemples ();
